f0f4634f78ad8f00c43c2a42fce0b4499a757c56
Roberto <gsanchez94@uabc.edu.mx>
2025-02-03 01:59:42 -0800

    remosion de react-json-view

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,20 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './styles/animations.css';
-import './styles/backgroundColors.css';
-import './styles/componentsStyles.css';
-import './styles/efects.css';
-import './styles/loaders.css';
-import './styles/positions.css';
-import './styles/scaleEffects.css';
-import './styles/sizes.css';
-import './styles/textColors.css';
-import './styles/backgroundGradients.css';
-import './styles/images.css';
-import './styles/fonts.css';
-import './styles/breacksPoints.css';
-import './styles/dinamicWidth.css';
+import './Styles/animations.css';
+import './Styles/backgroundColors.css';
+import './Styles/componentsStyles.css';
+import './Styles/efects.css';
+import './Styles/loaders.css';
+import './Styles/positions.css';
+import './Styles/scaleEffects.css';
+import './Styles/sizes.css';
+import './Styles/textColors.css';
+import './Styles/backgroundGradients.css';
+import './Styles/images.css';
+import './Styles/fonts.css';
+import './Styles/breacksPoints.css';
+import './Styles/dinamicWidth.css';
 
 import Tester from "./Pages/Tester";
 
@@ -27,7 +24,6 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" Component={Tester} />
-
       </Routes>
     </Router>
   );
